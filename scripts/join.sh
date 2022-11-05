a4cc295ec85283025156b740c603ac2e8b1f311f
S1M0N38 <bertolottosimone@gmail.com>
2022-11-05 11:41:48 +0100

    Join script working on linux and on mac
    Signed-off-by: S1M0N38 <bertolottosimone@gmail.com>

--- scripts/join.sh
+++ scripts/join.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-python join.py                                                 \
-  --quiet                                                      \
-  --input-files "$(date --date='yesterday' '+%Y%m%d')*.csv.gz" \
-  --input-dir "db-hour"                                        \
-  --output-file "$(date --date='yesterday' '+%Y%m%d').csv.gz"  \
-  --output-dir "db-day"                                        \
-  --remove                                                     \
-  --compress                                                   \
+set -e
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  input_files="$(date --date='yesterday' '+%Y%m%d')*.csv.gz"
+  output_file="$(date --date='yesterday' '+%Y%m%d').csv.gz"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  input_files="$(date -v -1d '+%Y%m%d')*.csv.gz"
+  output_file="$(date -v -1d '+%Y%m%d').csv.gz"
+else
+  echo "Unknown platform"
+fi
+
+python join.py                 \
+  --quiet                      \
+  --input-files "$input_files" \
+  --input-dir "db-hour"        \
+  --output-file "$output_file" \
+  --output-dir "db-day"        \
+  --remove                     \
+  --compress                   \
